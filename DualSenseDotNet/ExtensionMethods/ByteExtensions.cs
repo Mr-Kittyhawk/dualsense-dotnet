3e140497af1ca2afde2cad171896d0ea41a7eb8f
Nathan Lucci <Nathan.P.Lucci@pm.me>
2021-01-06 19:50:34 -0600

    Moved Project From Gitlab To Github

---
+++ DualSenseDotNet/ExtensionMethods/ByteExtensions.cs
@@ -0,0 +1,8 @@
+﻿
+namespace DualSenseDotNet {
+    public static class ByteExtensions {
+
+        /// <summary> Converts a <see cref="byte"/> to a <see cref="float"/> with a value between 0 and 1. </summary>
+        public static float ToNormalizedFloat(this byte @byte) => @byte / 255f;
+    }
+}
